84bf0e6a465c43cc0008b2dfe0d3056a3e884450
ookapratama <ookapratama2@gmail.com>
2024-03-23 05:04:28 +0800

    buat screen home (proses)

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,9 @@
 import {View, Image, StyleSheet, Alert} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {Container, HStack, IconNavigation, VStack} from '../../components';
 import {Text} from '@ui-kitten/components';
 import {LogOut} from '../../services/AuthService';
-import { navigate } from '../../navigation/RootNavigation';
+import {navigate} from '../../navigation/RootNavigation';
 
 const onLogOut = () => {
   Alert.alert('Warning', 'Ingin Logout Akun? ', [
@@ -14,19 +14,31 @@ const onLogOut = () => {
     {
       text: 'YA',
       onPress: async () => {
-        const status = await LogOut()
+        const status = await LogOut();
         if (status) {
-          console.log(status)
-          Alert.alert('Success', 'Berhasil Logout')
-          navigate('SignIn')
-        }
-        else Alert.alert('Warning', 'Terjadi kesalahan saat Logout')
+          console.log(status);
+          Alert.alert('Success', 'Berhasil Logout');
+          navigate('SignIn');
+        } else Alert.alert('Warning', 'Terjadi kesalahan saat Logout');
       },
     },
   ]);
 };
 
 const Home = () => {
+  const [activePasien, setActivePasien] = useState(true);
+  const [activeAntrian, setActiveAntrian] = useState(false);
+
+  const handleActvieMenu = (menuName: string) => {
+    if (menuName == 'Pasien') {
+      setActivePasien(true);
+      setActiveAntrian(false);
+    } else if (menuName == 'Antrian') {
+      setActiveAntrian(true);
+      setActivePasien(false);
+    }
+  };
+
   return (
     <Container level="1">
       <HStack level="4" padding={24} itemsCenter>
@@ -42,11 +54,16 @@ const Home = () => {
       </HStack>
       <HStack level="4" ph={24} pv={24} justify="flex-start" itemsCenter>
         <Text
-          style={[styles.menu, {marginRight: 24}]}
-          onPress={() => console.log('menu pasien')}>
+          style={[
+            [styles.menu, {color: activePasien ? 'black' : 'grey'}],
+            {marginRight: 24},
+          ]}
+          onPress={() => handleActvieMenu('Pasien')}>
           Pasien
         </Text>
-        <Text style={styles.menu} onPress={() => console.log('menu antrian')}>
+        <Text
+          style={[styles.menu, {color: activeAntrian ? 'black' : 'grey'}]}
+          onPress={() => handleActvieMenu('Antrian')}>
           Antrian
         </Text>
       </HStack>
